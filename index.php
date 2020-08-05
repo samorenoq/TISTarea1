6a5c4168245957cf5bcf9439bce7d26b2d45c172
samorenoq <samorenoq@eafit.edu.co>
2020-08-04 22:52:32 -0500

    Tarea 1 terminada

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 
     include 'db_connection.php';
 
+    //Obtener datos de la BD
+    $query = 'SELECT * FROM persona ORDER BY apellidos';
+    $resultado = mysqli_query($conn, $query);
+    $personas = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+    $nombres_apellidos = [];
+
+    foreach($personas as $persona)
+    {
+        $nombres_apellidos[] = $persona['nombres'] . ' ' .  $persona['apellidos'];
+    }
+
+    mysqli_free_result($resultado);
+
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +22,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarea 1 - Tópicos Especiales en Ingeniería de Software</title>
+    <title>Tarea 1</title>
 </head>
 <body>
+
+    <h2>Tarea 1 - Tópicos Especiales en Ingeniería de Software</h4>
+    
+    <a href="agregar.php">Agregar nombre</a>
+
+    <h4>Nombres registrados</h4>
+    <ul>
+        <?php foreach ($nombres_apellidos as $x): ?>
+            <li><?php echo htmlspecialchars($x) ?></li>
+        <?php endforeach; ?>
+    </ul>
     
 </body>
 </html>
